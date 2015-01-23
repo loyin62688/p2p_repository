f87e6f246b0782c90c48cdfdeeb465fd3ba65e64
loyin62688 <loyin@126.com>
2015-01-23 17:19:11 +0800

    model test

--- P2PADM/apps/P2Padmin/models.py
+++ P2PADM/apps/P2Padmin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class P2PServerInfo(models.Model):
+    project = models.CharField(max_length=50)
+    body_text = models.TextField()
+    col_date = models.DateTimeField()
+    relay_accnum = models.IntegerField()
+    p2p_accnum = models.IntegerField()
+    def __unicode__(self):
+        return self.headline
